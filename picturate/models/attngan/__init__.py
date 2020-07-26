1d7030bbae8c439ff45a0a18057971a4bfa318a5
Raman <raman@pop-os.localdomain>
2020-07-27 04:04:01 +0530

    Add init file for attngan

---
+++ picturate/models/attngan/__init__.py
@@ -0,0 +1,7 @@
+from picturate.models.attngan.attention import *
+from picturate.models.attngan.upsample import *
+from picturate.models.attngan.glu import *
+from picturate.models.attngan.conv import *
+from picturate.models.attngan.model import *
+
+from picturate.models.attngan.attention.AttentionGeneral as ATT_NET
