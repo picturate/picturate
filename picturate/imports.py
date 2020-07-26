7572ab5759a632a3bf2c7d7faac506256b7da1bd
Raman <raman@pop-os.localdomain>
2020-07-27 02:48:57 +0530

    Update imports: Add __future__ imports

--- picturate/imports.py
+++ picturate/imports.py
@@ -2,7 +2,10 @@
 import os
 import os.path as osp
 import errno
+import yaml
 from easydict import EasyDict as edict
+from __future__ import division
+from __future__ import print_function
 
 # Machine learning library imports
 import numpy as np
@@ -14,5 +17,10 @@ from PIL import Image, ImageDraw, ImageFont
 # Torch imports
 import torch
 import torch.nn as nn
+import torch.nn.parallel
 import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.nn import init
+from torchvision import models
+import torch.utils.model_zoo as model_zoo
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
