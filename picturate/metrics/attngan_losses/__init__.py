ceb513aa58ca8da8f0ce80188c41309b339d306f
Raman <raman@pop-os.localdomain>
2020-08-01 09:20:05 +0530

    Remove loss from metrics subpackage

--- picturate/metrics/attngan_losses/__init__.py
+++
@@ -1,3 +0,0 @@
-from picturate.metrics.attngan_losses.kl_loss import *
-from picturate.metrics.attngan_losses.image_text_loss import *
-from picturate.metrics.attngan_losses.text_image_loss import *
